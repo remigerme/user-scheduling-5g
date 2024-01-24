894d7b0f8729a71cfcaee9b4bcc42554358d3e4f
remi.germe <remi.germe@gmail.com>
2024-01-24 19:09:17 +0100

    instance using ndarray

---
+++ src/instance.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+import numpy as np
+
+@dataclass
+class Instance:
+    N: int
+    M: int
+    K: int
+    p: int
+
+    P: np.ndarray
+    R: np.ndarray
+
+
+def load_from_file(filename: str) -> Instance:
+    with open(filename, "r") as f:
+        lines = [l[3:-1] for l in f.readlines()]
+
+    N = int(float(lines[0]))
+    M = int(float(lines[1]))
+    K = int(float(lines[2]))
+    p = int(float(lines[3]))
+
+    P = np.zeros((N, M, K), dtype=int)
+    for n in range(N):
+        for k in range(K):
+            line = lines[3 + n * K + k].split("   ")
+            for (m, x) in enumerate(line):
+                P[n][m][k] = int(float(x)) 
+
+    R = np.zeros((N, M, K), dtype=int)
+    for n in range(N):
+        for k in range(K):
+            line = lines[3 + n * K + k].split("   ")
+            for (m, x) in enumerate(line):
+                P[n][m][k] = int(float(x)) 
+
+    return Instance(N, M, K, p, P, R)
