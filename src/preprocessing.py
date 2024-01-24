d20c52bf2e929ebcfb185472ac8dc0ea802ff380
remi.germe <remi.germe@gmail.com>
2024-01-24 19:09:28 +0100

    preprocessing structure

---
+++ src/preprocessing.py
@@ -0,0 +1,10 @@
+from instance import Instance
+
+def quick_preprocessing(instance: Instance) -> Instance:
+    ...
+
+def ip_dominated_processing(instance: Instance) -> Instance:
+    ...
+
+def lp_dominated_processing(instance: Instance) -> Instance:
+    ...
