35d993e74da734e5b72f595a3787b61fbb65d99b
remi.germe <remi.germe@gmail.com>
2024-01-28 15:50:19 +0100

    quick processing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,10 +1,22 @@
+import numpy as np
+
 from instance import Instance
 
-def quick_preprocessing(instance: Instance) -> Instance:
+
+INF = int(1e10)
+
+
+def quick_preprocessing(instance: Instance):
+    for n in range(instance.N):
+        for m in range(instance.M):
+            for k in range(instance.K):
+                if instance.P[n][m][k] > instance.p:
+                    instance.P[n][m][k] = INF
+                    instance.R[n][m][k] = 0
+
+
+def ip_dominated_processing(instance: Instance):
     ...
 
-def ip_dominated_processing(instance: Instance) -> Instance:
-    ...
-
-def lp_dominated_processing(instance: Instance) -> Instance:
+def lp_dominated_processing(instance: Instance):
     ...
