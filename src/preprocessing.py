6d5f9b1520ce1e8de7dd7f45dec46c79eb83433f
remi.germe <remi.germe@gmail.com>
2024-01-28 17:45:15 +0100

    code clarity and remove function

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -6,34 +6,37 @@ from instance import Instance
 INF = int(1e10)
 
 
+def remove_from(instance: Instance, n: int, k: int, m: int):
+    instance.P[n][k][m] = INF
+    instance.R[n][k][m] = 0
+
+
 def quick_preprocessing(instance: Instance):
     for n in range(instance.N):
         for k in range(instance.K):
             for m in range(instance.M):
                 if instance.P[n][k][m] > instance.p:
-                    instance.P[n][k][m] = INF
-                    instance.R[n][k][m] = 0
+                    remove_from(instance, n, k, m)
 
 
 def ip_dominated_processing(instance: Instance):
     for n in range(instance.N):
         arg_sorted_Pkm = np.unravel_index(np.argsort(instance.P[n], axis = None), instance.P[n].shape)
         arg_sorted_Rkm = np.unravel_index(np.argsort(instance.R[n], axis = None), instance.R[n].shape)
+        pkm_list = list(zip(*arg_sorted_Pkm))
         rkm_list = list(zip(*arg_sorted_Rkm))
         ir = 0
         removed = set()
-        for (pk, pm) in zip(*arg_sorted_Pkm):
+        for (pk, pm) in pkm_list:
             if (pk, pm) in removed:
                 continue
             while rkm_list[ir] != (pk, pm):
                 removed.add(rkm_list[ir])
                 ir += 1
-            if rkm_list[ir] == (pk, pm):
-                ir += 1
+            ir += 1
 
         for (k, m) in removed:
-            instance.P[n][k][m] = INF
-            instance.R[n][k][m] = 0
+            remove_from(instance, n, k, m)
 
 
 def lp_dominated_processing(instance: Instance):
