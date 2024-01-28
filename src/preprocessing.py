18e14ec5a2078422f57abe5c51b9b250a1520dc7
remi.germe <remi.germe@gmail.com>
2024-01-28 17:33:59 +0100

    changing from MK to KM

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -8,15 +8,33 @@ INF = int(1e10)
 
 def quick_preprocessing(instance: Instance):
     for n in range(instance.N):
-        for m in range(instance.M):
-            for k in range(instance.K):
-                if instance.P[n][m][k] > instance.p:
-                    instance.P[n][m][k] = INF
-                    instance.R[n][m][k] = 0
+        for k in range(instance.K):
+            for m in range(instance.M):
+                if instance.P[n][k][m] > instance.p:
+                    instance.P[n][k][m] = INF
+                    instance.R[n][k][m] = 0
 
 
 def ip_dominated_processing(instance: Instance):
-    ...
+    for n in range(instance.N):
+        arg_sorted_Pkm = np.unravel_index(np.argsort(instance.P[n], axis = None), instance.P[n].shape)
+        arg_sorted_Rkm = np.unravel_index(np.argsort(instance.R[n], axis = None), instance.R[n].shape)
+        rkm_list = list(zip(*arg_sorted_Rkm))
+        ir = 0
+        removed = set()
+        for (pk, pm) in zip(*arg_sorted_Pkm):
+            if (pk, pm) in removed:
+                continue
+            while rkm_list[ir] != (pk, pm):
+                removed.add(rkm_list[ir])
+                ir += 1
+            if rkm_list[ir] == (pk, pm):
+                ir += 1
+
+        for (k, m) in removed:
+            instance.P[n][k][m] = INF
+            instance.R[n][k][m] = 0
+
 
 def lp_dominated_processing(instance: Instance):
     ...
