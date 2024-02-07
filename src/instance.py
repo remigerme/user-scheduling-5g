007a99296faf2939ed35b015cb044dab4eaad8af
remi.germe <remi.germe@gmail.com>
2024-02-07 11:10:38 +0100

    storing removed triplets and sort pkm

--- src/instance.py
+++ src/instance.py
@@ -11,6 +11,22 @@ class Instance:
     P: np.ndarray
     R: np.ndarray
 
+    # Contains triplets (n, k, m) which were preprocessed
+    removed: set[tuple[int, int, int]]
+
+    def get_sorted_pkm(self) -> list[list[tuple[int, int]]]:
+        """
+        Returns a list L of length N such as L[n]
+        contains couples (k, m) ordered by increasing
+        values of P[n][k][m].
+        """
+        pkm_lists = []
+        for n in range(self.N):
+            arg_sorted_Pkm = np.unravel_index(np.argsort(self.P[n], axis = None), self.P[n].shape)
+            pkm_list = list(zip(*arg_sorted_Pkm))
+            pkm_lists.append(pkm_list)
+        return pkm_lists
+
 
 def load_from_file(filename: str) -> Instance:
     with open(filename, "r") as f:
