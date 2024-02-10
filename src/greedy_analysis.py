e6162d6b3ad1c508c822b475d5f2b091f778b5ad
remi.germe <remi.germe@gmail.com>
2024-02-10 11:37:52 +0100

    formatting

--- src/greedy_analysis.py
+++ src/greedy_analysis.py
@@ -1,10 +1,12 @@
 import numpy as np
 from scipy.optimize import linprog, OptimizeResult
+from matplotlib.pyplot import title, legend, show, scatter, xlim, xlabel, ylabel, yscale
 
 from instance import Instance, load_from_file
 from solution import Solution
 from preprocessing import preprocess
 from greedy import greedy
+
 
 def get_c(instance: Instance) -> np.ndarray:
     """
@@ -17,6 +19,7 @@ def get_c(instance: Instance) -> np.ndarray:
             for m in range(instance.M - 1, 0, -1):
                 R[n][k][m] = R[n][k][m] - R[n][k][m - 1]
     return np.ndarray.flatten(R)
+
 
 def get_A(instance: Instance) -> np.ndarray:
     """
@@ -31,6 +34,7 @@ def get_A(instance: Instance) -> np.ndarray:
     A_vector = np.ndarray.flatten(P)
     return np.reshape(A_vector, (1, instance.N * instance.K * instance.M))
 
+
 def solve(instance: Instance) -> OptimizeResult:
     """
     Use this function without preprocessing !
@@ -39,4 +43,46 @@ def solve(instance: Instance) -> OptimizeResult:
     A = get_A(instance)
     b = np.full(1, instance.p, dtype=int)
     # Careful : linprog minimizes, we want to maximize
-    return linprog(- c, A_ub=A, b_ub=b, bounds=(0, 1))
+    return linprog(-c, A_ub=A, b_ub=b, bounds=(0, 1))
+
+
+def data_rate(instance: Instance, solution: Solution) -> float:
+    s = 0
+    for n in range(instance.N):
+        for k in range(instance.K):
+            for m in range(instance.M):
+                s += solution.X[n][k][m] * instance.R[n][k][m]
+    return s
+
+
+def power_used(instance: Instance, solution: Solution) -> float:
+    p = 0
+    for n in range(instance.N):
+        for k in range(instance.K):
+            for m in range(instance.M):
+                p += solution.X[n][k][m] * instance.P[n][k][m]
+    return p
+
+
+def show_graph():
+    scale = [30, 10]
+    for n in range(5):
+        i = load_from_file(f"testfiles/test{n + 1}.txt")
+        solved = solve(i)
+        preprocess(i)
+        s = greedy(i)
+        abs = [power_used(i, s) / i.p, 1 - solved.slack[0] / i.p]
+        ords = [data_rate(i, s), -solved.fun]
+        print(n, abs, ords)
+        scatter(abs, ords, s=scale, label=f"Test {n + 1}")
+    title("")
+    xlim(0.9, xlim()[1])
+    yscale("log")
+    xlabel("Power used / Total available power")
+    ylabel("Total data rate")
+    legend()
+    show()
+
+
+if __name__ == "__main__":
+    show_graph()
