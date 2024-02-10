a639ab382eed1a631b8238582e768006b6e57091
remi.germe <remi.germe@gmail.com>
2024-02-10 19:31:42 +0100

    clean plotting with cpu time

--- src/greedy_analysis.py
+++ src/greedy_analysis.py
@@ -1,6 +1,8 @@
+from time import time
+
 import numpy as np
 from scipy.optimize import linprog, OptimizeResult
-from matplotlib.pyplot import title, legend, show, scatter, xlim, xlabel, ylabel, yscale
+import matplotlib.pyplot as plt
 
 from instance import Instance, load_from_file
 from solution import Solution
@@ -66,22 +68,50 @@ def power_used(instance: Instance, solution: Solution) -> float:
 
 def show_graph():
     scale = [30, 10]
+    fig, (rates, times) = plt.subplots(1, 2)
+    categories = ["Greedy", "LP solver"]
+    x_positions = range(len(categories))
     for n in range(5):
         i = load_from_file(f"testfiles/test{n + 1}.txt")
+        lp_start = time()
         solved = solve(i)
+        lp_duration = time() - lp_start
         preprocess(i)
+        greedy_start = time()
         s = greedy(i)
+        greedy_duration = time() - greedy_start
         abs = [power_used(i, s) / i.p, 1 - solved.slack[0] / i.p]
         ords = [data_rate(i, s), -solved.fun]
-        print(n, abs, ords)
-        scatter(abs, ords, s=scale, label=f"Test {n + 1}")
-    title("")
-    xlim(0.9, xlim()[1])
-    yscale("log")
-    xlabel("Power used / Total available power")
-    ylabel("Total data rate")
-    legend()
-    show()
+        rates.scatter(abs, ords, s=scale, label=f"Test {n + 1}")
+        if n >= 3:
+            times.plot(
+                x_positions, [greedy_duration, lp_duration], label=f"Test {n + 1}"
+            )
+        print(
+            (
+                f"Test {n + 1} :\n"
+                f"Power usage (greedy, lp): {abs}\n"
+                f"Data rates (idem) : {ords}\n"
+                f"CPU time (s) (idem) : {greedy_duration}, {lp_duration}\n"
+            )
+        )
+
+    # Plotting rates
+    rates.set_title("Greedy (wide dots) vs\nScipy LP solver (small dots)")
+    rates.set_xlim(0.7, rates.get_xlim()[1])
+    rates.set_yscale("log")
+    rates.set_xlabel("Power used / Total available power")
+    rates.set_ylabel("Total data rate")
+    rates.legend()
+
+    # Plotting times
+    times.set_title("CPU time")
+    times.set_xticks(x_positions, categories)
+    times.set_yscale("log")
+    times.set_ylabel("Time elapsed (s)")
+    times.legend()
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == "__main__":
