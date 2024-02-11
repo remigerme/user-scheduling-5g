b318226905dde16a4652846348cf22055e052fb6
remi.germe <remi.germe@gmail.com>
2024-02-11 12:20:37 +0100

    some bug fixes

--- src/dp.py
+++ src/dp.py
@@ -6,8 +6,8 @@ from solution import Solution
 
 def dp(instance: Instance) -> Solution:
     p_max, N, K, M = instance.p, instance.N, instance.K, instance.M
-    D = np.zeros((p + 1, N + 1, K + 1, M + 1), dtype=int)
-    M = np.zeros((p + 1, N + 1, K + 1, M + 1), dtype=int)
+    D = np.zeros((p_max + 1, N + 1, K + 1, M + 1), dtype=int)
+    PM = np.zeros((p_max + 1, N + 1, K + 1), dtype=int)
     for p in range(1, p_max + 1):
         for n in range(1, N + 1):
             D[p][n][0][M] = D[p][n - 1][K][M]
@@ -18,13 +18,13 @@ def dp(instance: Instance) -> Solution:
                     current_cost = instance.P[n - 1][k - 1][m - 1]
                     if current_cost <= p:
                         increment_value = instance.R[n - 1][k - 1][m - 1]
-                        if M[p][n][k] > 0:
-                            increment_value -= instance.R[n - 1][k - 1][M[p][n][k] - 1]
+                        if PM[p][n][k] > 0:
+                            increment_value -= instance.R[n - 1][k - 1][PM[p][n][k] - 1]
 
                         current_value = D[p - current_cost][N][K][M] + increment_value
                         if current_value >= previous_value:
                             D[p][n][k][m] = current_value
-                            M[p][n][k] = m
+                            PM[p][n][k] = m
                     else:
-                        # No need to update M[p][n][k]
+                        # No need to update PM[p][n][k]
                         D[p][n][k][m] = previous_value
